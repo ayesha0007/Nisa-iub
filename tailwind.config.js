8c0f4f3f92b85247eb6d6c3d742c7e420c009547
borno <mdborno44@gmail.com>
2024-06-22 20:20:33 +0600

    Your commit message

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#DFF2F9',
+      },
+    },
+  },
+  plugins: [],
+}
+
